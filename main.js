79df9272ad884f3cf6432204a27a3961fa2d7f0d
Jonathan Schupak <jrschupak@gmail.com>
2016-03-03 13:43:36 -0500

    new board code, boards array

--- main.js
+++ main.js
@@ -1,24 +1,60 @@
 var space = document.querySelectorAll("space");
+//
+//
+//
+// var insertImgTag = function() {
+//   for(var i = 0; i < space.length; i += 1) {
+//     var img = document.createElement('img');
+//     space[i].appendChild(img)
+//     console.log("This is space i " + space[i]);
+//   }
+//   return space[i];
+// }
+//   insertImgTag();
+//
+//   var img = document.querySelectorAll('img');
+//
+//   var insertImg = function() {
+//     for(var i = 0; i < img.length; i += 1) {
+//       img[i].src = 'images/zombieSpace.jpeg';
+//     }
+//     return img[i];
+//   }
+//
+//   insertImg();
 
+var boardSpacesArr = [];
 
+var rowDivs = function() {
+  var rows = document.querySelectorAll('.row');
 
-var insertImgTag = function() {
-  for(var i = 0; i < space.length; i += 1) {
-    var img = document.createElement('img');
-    space[i].appendChild(img)
-    console.log("This is space i " + space[i]);
+  for(var i = 0; i < rows.length; i += 1) {
+    boardSpacesArr.push(rows[i]);
   }
-  return space[i];
+  return rows[i];
 }
-  insertImgTag();
 
-  var img = document.querySelectorAll('img');
+rowDivs();
 
-  var insertImg = function() {
-    for(var i = 0; i < img.length; i += 1) {
-      img[i].src = 'images/zombieSpace.jpeg';
-    }
-    return img[i];
-  }
 
-  insertImg();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Do Not Remove This.
