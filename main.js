9aa24af9144325e58afbc2e9f86baad0d19ba123
Jonathan Schupak <jrschupak@gmail.com>
2016-03-03 10:13:51 -0500

    updated JS insert img to space

---
+++ main.js
@@ -0,0 +1,13 @@
+var space = document.querySelectorAll("space");
+
+
+
+var insertImg = function() {
+  for(var i = 0; i < space.length; i += 1) {
+    var img = document.createElement('img');
+    space[i].appendChild(img)
+    console.log("This is space i " + space[i]);
+  }
+  return space[i];
+}
+  insertImg();
